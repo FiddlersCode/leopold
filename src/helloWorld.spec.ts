f51415f50bad82555993096e8419bf436f5bc1a5
Paula Muldoon <paula.muldoon@camcog.com>
2019-12-15 16:46:13 +0000

    issue-5: fix deliberate test failure

--- src/helloWorld.spec.ts
+++ src/helloWorld.spec.ts
@@ -3,7 +3,7 @@ import Program from './helloWorld';
 
 describe('hello world', () => {
   it('returns hello world', () => {
-    const expected = 'potato';
+    const expected = 'hello world';
     const actual = Program.sayHi();
     expect(expected).toBe(actual);
   });
