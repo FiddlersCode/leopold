91fc7ed8839a08150b7d7cdeed3fe999db52399b
Paula Muldoon <paula.muldoon@camcog.com>
2019-12-15 09:02:47 +0000

    issue-3: remove commented lint config and fix main filename in package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,39 +37,9 @@ module.exports = {
             "@typescript-eslint/parser": [".ts", ".tsx"]
         },
         "import/resolver": {
-            // use <root>/tsconfig.json
             "typescript": {
                 "alwaysTryTypes": true
             },
-
-
-            //     // use <root>/path/to/folder/tsconfig.json
-            //     "typescript": {
-            //         "directory": "./path/to/folder"
-            //     },
-            //
-            //     // Multiple tsconfigs (Useful for monorepos)
-            //
-            //     // use a glob pattern
-            //     "typescript": {
-            //         "directory": "./packages/*/tsconfig.json"
-            //     },
-            //
-            //     // use an array
-            //     "typescript": {
-            //         "directory": [
-            //             "./packages/module-a/tsconfig.json",
-            //             "./packages/module-b/tsconfig.json"
-            //         ]
-            //     },
-            //
-            //     // use an array of glob patterns
-            //     "typescript": {
-            //         "directory": [
-            //             "./packages/*/tsconfig.json",
-            //             "./other-packages/*/tsconfig.json"
-            //         ]
-            //     }
         },
         "import/extensions": [
             ".js",
