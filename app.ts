bb130fe88b2e65325e8b61ae0611873f7b36b849
Paula Muldoon <paula.muldoon@camcog.com>
2020-01-20 10:46:18 +0000

    issue-39: fix error with Dockerfile cmd, add start-dev script for pre-push dry run

--- app.ts
+++ app.ts
@@ -1,10 +1,27 @@
 import {LeopoldMongoClientWrapper} from "./src/mongo/leopoldMongoClientWrapper";
 const mongoClient = new LeopoldMongoClientWrapper();
+import * as express from 'express';
+
+const startWebApp = () => {
+    const app = express();
+    const port: number = parseInt(process.env.PORT) || 8080;
+    app.get('/healthcheck', (req, res) => {
+        res.send('Service is healthy!\n');
+    });
+
+    app.listen(port, process.env.HOST);
+    console.log(`Running on http://${process.env.HOST}:${process.env.PORT}`);
+};
+
+
 
 const startServices = async() => {
     if (process.env.NODE_ENV === "production") {
-        process.env.MONGO_URI = "mongodb://192.168.0.34:27017/leopold"
+        process.env.MONGO_URI = "mongodb://192.168.0.34:27017/leopold";
+        process.env.PORT = '8080';
+        process.env.HOST = "192.168.0.34"
     }
+    startWebApp();
     await mongoClient.connectToDB("leopold");
 };
 
